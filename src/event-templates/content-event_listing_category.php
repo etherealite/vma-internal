3e002e8d9725d281461e390173a92db56b4c5ae3
Evan Bangham <seclayer@gmail.com>
2022-09-03 21:42:55 -0700

    version 0.0.1 release

---
+++ src/event-templates/content-event_listing_category.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * The template for displaying archive.
+ */
+
+get_header();
+
+global $wp_query;
+?>
+
+<div class="vma-event-hero">
+    <?php echo do_shortcode('[elementor-template id="30803"]'); ?>
+</div>
+<div class="wpem-container">
+<div class="wpem-main wpem-event-listing-type-page">
+<div class="wpem-row">
+
+<div class="wpem-col wpem-col-12 wpem-col-md-4 wpem-col-lg-3 wpem-col-xl-3">
+        <div class="vma-event-sidebar">
+            <?php echo do_shortcode('[elementor-template id="30800"]'); ?>
+        </div>
+</div>
+<div class="wpem-col-12 wpem-col-md-8 wpem-col-lg-9 wpem-col-xl-9 wpem-event-listing-type-page-wrapper">
+
+<div class="wpem-event-listing-type-page-title">
+    <h2 class="vma-event-archive-title__text"><?php echo get_the_archive_title(); ?></h2>
+    <h4 class="vma-event-archive-title__description"><?php echo wp_kses_post(term_description()); ?></h4>
+</div>
+
+        <?php
+        // remove calender view
+        remove_action('end_event_listing_layout_icon', 'add_event_listing_calendar_layout_icon');
+        ?>
+            <div class="event_listings">
+                <?php if ( have_posts() ) : ?>
+
+                    <?php get_event_manager_template( 'event-listings-start.php' ,array('layout_type'=>'all')); ?>           
+
+                    <?php while ( have_posts() ) : the_post(); ?>
+
+                        <?php  get_event_manager_template_part( 'content', 'event_listing' ); ?>
+                        
+                    <?php endwhile; ?>
+
+                    <?php get_event_manager_template( 'event-listings-end.php' ); ?>
+
+                    <?php get_event_manager_template( 'pagination.php', array( 'max_num_pages' => $wp_query->max_num_pages ) ); ?>
+
+                <?php else :
+
+                    do_action( 'event_manager_output_events_no_results' );
+
+                endif;
+
+                wp_reset_postdata(); ?>                
+            </div>
+</div>
+</div>
+</div>
+</div>
+
+
+<?php get_footer(); ?>
---
+++ vma-internal.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * Plugin Name: VMA Internal Plugin
+ * Description: General purpose plugin for site specific integrations.
+ * Author: Evan Bangham
+ * Version: 0.0.1
+ * Author URI: https://github.com/etherealite
+ *
+ * Text Domain: VMA
+ **/
+
+
+class Vma_Internal {
+
+    public const VERSION = '0.0.1';
+
+    private string $path;
+
+    public function __construct($pluginPath) {
+        $this->path = $pluginPath;
+    }
+
+    public static function bootstrap(): self
+    {
+        $instance = new static(__DIR__);
+        $instance->register();
+        return $instance;
+    }
+
+    public function register(): void
+    {
+        add_filter(
+            'register_taxonomy_event_listing_category_args', 
+            [$this, 'filter_register_taxonomy_event_listing_category_args'],
+        10, 1);
+
+        add_filter(
+            'register_taxonomy_event_listing_type_args', 
+            [$this, 'filter_register_taxonomy_event_listing_type_args'],
+        10, 1);
+
+        add_filter(
+            'event_manager_locate_template',
+            [$this, 'filter_event_manager_locate_template'],
+        10, 3);
+
+        add_action(
+            'plugins_loaded',
+            [$this, 'action_plugins_loaded'],
+        10, 0);
+    }
+
+    public function filter_register_taxonomy_event_listing_category_args(
+        array $args
+    ): array
+    {
+        $rewrite = [
+            'with_front'   => false,
+            'hierarchical' => false
+        ];
+        $args['rewrite'] = $rewrite;
+
+        return $args;
+    }
+
+    public function filter_register_taxonomy_event_listing_type_args(
+        array $args
+    ): array 
+    {
+        $rewrite = [
+            'with_front'   => false,
+            'hierarchical' => false
+        ];
+        $args['rewrite'] = $rewrite;
+        return $args;
+    }
+
+    public function filter_event_manager_locate_template(
+        $template, $template_name, $template_path
+    ) {
+
+        // Look for tempaltes in theme on the passed path
+
+        $themeTemplate = locate_template(
+
+            array(
+
+                trailingslashit($template_path) . $template_name,
+
+                $template_name
+            )
+        );
+
+        if($themeTemplate) {
+            return $themeTemplate;
+        }
+
+        // Get the template from the VMA plugin
+        $internal_path = $this->path . '/src/event-templates/';
+
+        $internalTemplate = trailingslashit($internal_path) . $template_name;
+
+        if (file_exists($internalTemplate)) {
+            return $internalTemplate;
+        }
+
+        // return what event manager found.
+        return $template;
+    }
+
+    public function action_plugins_loaded(): void
+    {
+        remove_filter(
+            'archive_template',
+            [WP_Event_Manager_Post_Types::instance(), 'event_archive'],
+        20);
+
+        add_filter(
+            'archive_template',
+            [$this, 'filter_archive_template']
+        );
+
+        add_action(
+            'wp_enqueue_scripts', 
+            [$this, 'action_wp_enqueue_scripts'],
+        50);
+    }
+
+    public function filter_archive_template(): string
+    {
+		if (is_tax( 'event_listing_category')) {
+
+			$template = $this->path . '/src/event-templates/content-event_listing_category.php';
+	    }
+	    elseif (is_tax( 'event_listing_type')) {
+
+			$template = $this->path . '/src/event-templates/content-event_listing_type.php';
+	    }
+
+	    return $template;
+    }
+
+    public function action_wp_enqueue_scripts(): void
+    {
+        wp_enqueue_style(
+            'vma-internal-frontend',
+            plugin_dir_url(__FILE__) . 'src/styles.css',
+            [],
+            static::VERSION
+        );
+    }
+}
+
+
+(function() {
+    $instance = Vma_Internal::bootstrap();
+
+    register_deactivation_hook(__FILE__, function() use($instance) {
+        delete_option('rewrite_rules');
+    });
+
+    register_activation_hook(__FILE__, function() {
+        add_action('init', function() {
+            flush_rewrite_rules();
+        }, 10);
+    });
+})();
+
+
+
